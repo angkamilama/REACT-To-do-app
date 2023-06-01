4175516b76339cd3b38dd55ddd65e943487b42f2
angkamilama <angkami46@gmail.com>
2023-06-01 23:10:47 +0200

    add edit button and add media queries

--- src/App.js
+++ src/App.js
@@ -7,21 +7,48 @@ import { v4 as uuidv4 } from "uuid";
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todoTopic, setTodoTopic] = useState([]);
+  const [showPop, setShowPop] = useState(false);
+  const [removeId, setRemoveId] = useState("");
+  const [editId, setEditId] = useState(0);
 
-  const handleSubmit = (e, value) => {
-    console.log(value);
+  const handleSubmit = (e) => {
     e.preventDefault();
     let idNum = uuidv4();
+
     if (inputValue === "") {
       alert("Please enter a task!!!");
+    } else if (editId) {
+      const editedTodoTopic = todoTopic.map((todo) =>
+        todo.id === editId ? { ...todo, name: inputValue } : todo
+      );
+      setTodoTopic(editedTodoTopic);
+      setEditId(0);
+      setInputValue("");
     } else {
-      console.log(inputValue);
       setTodoTopic((todoTopic) => [
         ...todoTopic,
         { name: inputValue, id: idNum },
       ]);
       setInputValue("");
     }
+  };
+
+  const RemoveTopic = () => {
+    setShowPop((showPop) => !showPop);
+    const updatedTodoTopic = todoTopic.filter((todo) => todo.id !== removeId);
+    setTodoTopic(updatedTodoTopic);
+  };
+
+  const showContents = (id) => {
+    setShowPop((showPop) => !showPop);
+    setRemoveId((removeId) => id);
+    setInputValue("");
+  };
+
+  const handleEdit = (id) => {
+    const editedTodo = todoTopic.find((todo) => todo.id === id);
+    setInputValue((inputValue) => editedTodo.name);
+    setEditId((editId) => id);
   };
 
   return (
@@ -38,32 +65,52 @@ function App() {
               onChange={(e) => setInputValue(e.target.value)}
             />
           </label>
-          <button type="submit">ADD</button>
+          <button type="submit">{editId ? "EDIT" : "ADD"}</button>
         </form>
-      </div>
-      <TodoList handleSubmit={handleSubmit} topics={todoTopic} />
-      {/* {showPop && (
-        <div className="modal-background">
-          <div className="modal-container">
-            <div className="modal-title">
-              <p> Are you sure that you want to remove the task ?</p>
-            </div>
-            <div className="modal-btns">
-              <button
-                className="topic-remove btn-no"
-                onClick={() => {
-                  showContents();
-                }}
-              >
-                No
-              </button>
-              <button className="topic-remove" onClick={() => RemoveTopic()}>
-                Yes
-              </button>
-            </div>
+        <div className="lists">
+          <div className="topics">
+            {todoTopic.map((todo) => (
+              <div className="topic" key={todo.id}>
+                <div className="topic-heading">{todo.name}</div>
+                <button
+                  className="topic-edit"
+                  onClick={() => handleEdit(todo.id)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="topic-remove"
+                  onClick={() => showContents(todo.id)}
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
           </div>
         </div>
-      )} */}
+        {showPop && (
+          <div className="modal-background">
+            <div className="modal-container">
+              <div className="modal-title">
+                <p> Are you sure that you want to remove the task ?</p>
+              </div>
+              <div className="modal-btns">
+                <button
+                  className="topic-remove btn-no"
+                  onClick={() => {
+                    showContents();
+                  }}
+                >
+                  No
+                </button>
+                <button className="topic-remove" onClick={() => RemoveTopic()}>
+                  Yes
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
