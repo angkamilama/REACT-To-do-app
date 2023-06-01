803faa67a32aa511cfe30f1fd17cda73ac28e81b
angkamilama <angkami46@gmail.com>
2023-06-01 21:06:10 +0200

    add edit button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import style from "./Style.css";
 import { useState } from "react";
+import TodoList from "./components/TodoList";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [todoTopic, setTodoTopic] = useState([]);
-  const [showPop, setShowPop] = useState(false);
-  const [removeId, setRemoveId] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e, value) => {
+    console.log(value);
     e.preventDefault();
     let idNum = uuidv4();
     if (inputValue === "") {
@@ -22,17 +22,6 @@ function App() {
       ]);
       setInputValue("");
     }
-  };
-
-  const RemoveTopic = () => {
-    setShowPop((showPop) => !showPop);
-    const updatedTodoTopic = todoTopic.filter((todo) => todo.id !== removeId);
-    setTodoTopic(updatedTodoTopic);
-  };
-
-  const showContents = (val) => {
-    setShowPop((showPop) => !showPop);
-    setRemoveId((removeId) => val);
   };
 
   return (
@@ -51,48 +40,30 @@ function App() {
           </label>
           <button type="submit">ADD</button>
         </form>
-        <div className="lists">
-          <div className="topics">
-            {todoTopic.map((list) => {
-              return (
-                <div className="topic-background">
-                  <div className="topic" key={list.id}>
-                    <div className="topic-heading">{list.name}</div>
-                    <button
-                      className="topic-details"
-                      onClick={() => showContents(list.id)}
-                    >
-                      Remove
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        {showPop && (
-          <div className="modal-background">
-            <div className="modal-container">
-              <div className="modal-title">
-                <p> Are you sure that you want to remove the task ?</p>
-              </div>
-              <div className="modal-btns">
-                <button
-                  className="topic-remove btn-no"
-                  onClick={() => {
-                    showContents();
-                  }}
-                >
-                  No
-                </button>
-                <button className="topic-remove" onClick={() => RemoveTopic()}>
-                  Yes
-                </button>
-              </div>
+      </div>
+      <TodoList handleSubmit={handleSubmit} topics={todoTopic} />
+      {/* {showPop && (
+        <div className="modal-background">
+          <div className="modal-container">
+            <div className="modal-title">
+              <p> Are you sure that you want to remove the task ?</p>
+            </div>
+            <div className="modal-btns">
+              <button
+                className="topic-remove btn-no"
+                onClick={() => {
+                  showContents();
+                }}
+              >
+                No
+              </button>
+              <button className="topic-remove" onClick={() => RemoveTopic()}>
+                Yes
+              </button>
             </div>
           </div>
-        )}
-      </div>
+        </div>
+      )} */}
     </div>
   );
 }
